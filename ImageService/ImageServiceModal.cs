e9502999c02efd44cf24b5462e4797ae55ab0019
braunsteind <33955950+braunsteind@users.noreply.github.com>
2018-04-10 16:47:45 +0300

    Modal

--- ImageService/ImageServiceModal.cs
+++ ImageService/ImageServiceModal.cs
@@ -15,12 +15,37 @@ namespace ImageService.Modal
     public class ImageServiceModal : IImageServiceModal
     {
         #region Members
+
         private string m_OutputFolder;            // The Output Folder
         private int m_thumbnailSize;              // The Size Of The Thumbnail Size
 
+        public ImageServiceModal(string m_OutputFolder, int m_thumbnailSize)
+        {
+            this.m_OutputFolder = m_OutputFolder;
+            this.m_thumbnailSize = m_thumbnailSize;
+        }
+
         public string AddFile(string path, out bool result)
         {
-            throw new NotImplementedException();
+            //check the path exists
+            if (File.Exists(path))
+            {
+                //temp
+                result = true;
+                return "temp";
+            }
+            else
+            {
+                result = false;
+                return "Failed";
+            }
+        }
+
+        public void CreateDir(string path)
+        {
+            //if output folder doesn't exist, create it
+            DirectoryInfo di = Directory.CreateDirectory(this.m_OutputFolder);
+            DateTime dt = File.GetCreationTime(path);
         }
 
         #endregion
