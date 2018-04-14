c63025e1aefdf9d9c2b080dde60286f4f2ce7be3
ShlomiZ <shlomi.z1991@gmail.com>
2018-04-14 20:32:35 +0300

    Updates

--- ImageService/ImageService/Modal/Event/CommandRecievedEventArgs.cs
+++ ImageService/ImageService/Modal/Event/CommandRecievedEventArgs.cs
@@ -12,6 +12,12 @@ namespace ImageService.Modal
         public string[] Args { get; set; }
         public string RequestDirPath { get; set; }  // The Request Directory
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="id"> id of the command</param>
+        /// <param name="args"> command arguments </param>
+        /// <param name="path"> path of requested dir </param>
         public CommandRecievedEventArgs(int id, string[] args, string path)
         {
             CommandID = id;
--- ImageService/ImageService/Modal/Event/DirectoryCloseEventArgs.cs
+++ ImageService/ImageService/Modal/Event/DirectoryCloseEventArgs.cs
@@ -8,10 +8,21 @@ namespace ImageService.Modal
 {
     public class DirectoryCloseEventArgs : EventArgs
     {
+        /// <summary>
+        /// Set & Get of directory path
+        /// </summary>
         public string DirectoryPath { get; set; }
 
+        /// <summary>
+        /// Set & Get of message
+        /// </summary>
         public string Message { get; set; }             // The Message That goes to the logger
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="dirPath"> directory name </param>
+        /// <param name="message"> the message </param>
         public DirectoryCloseEventArgs(string dirPath, string message)
         {
             DirectoryPath = dirPath;                    // Setting the Directory Name
