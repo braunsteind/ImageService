7057b07c51f12bbdd29931dcddd5843de7c5b40c
braunsteind <33955950+braunsteind@users.noreply.github.com>
2018-05-06 16:17:20 +0300

    GUI

--- ImageServiceGUI/Model/ILogModel.cs
+++ ImageServiceGUI/Model/ILogModel.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace ImageServiceGUI.Model
 {
-    class SettingsModel : ISettingsModel
+    class ILogModel
     {
-
     }
 }
--- ImageServiceGUI/ViewModel/SettingsViewModel.cs
+++ ImageServiceGUI/ViewModel/SettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageServiceGUI.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace ImageServiceGUI.ViewModel
 {
     class SettingsViewModel
     {
+        private ISettingsModel settingsModel;
     }
 }
