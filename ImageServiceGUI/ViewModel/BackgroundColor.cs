8bc24e4e82d4f3fe5890f7949bf4cc9af256e332
braunsteind <33955950+braunsteind@users.noreply.github.com>
2018-05-24 09:51:30 +0100

    Log

---
+++ ImageServiceGUI/ViewModel/BackgroundColor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ImageServiceGUI.ViewModel
+{
+    class BackgroundColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType.Name != "Brush")
+            {
+                throw new InvalidOperationException("Not brush!");
+            }
+
+            //check type
+            string type = (string)value;
+            if (type == "INFO")
+            {
+                return System.Windows.Media.Brushes.LightGreen;
+            }
+            if (type == "WARNING")
+            {
+                return System.Windows.Media.Brushes.Yellow;
+            }
+            if (type == "FAIL")
+            {
+                return System.Windows.Media.Brushes.Coral;
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
