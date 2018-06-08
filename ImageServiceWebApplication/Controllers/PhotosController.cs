e3cfe8199bab9df8e60d967a3b0be156911e9c9e
braunsteind <33955950+braunsteind@users.noreply.github.com>
2018-06-08 21:14:14 +0300

    Photos and bugs fix

--- ImageServiceWebApplication/Controllers/PhotosController.cs
+++ ImageServiceWebApplication/Controllers/PhotosController.cs
@@ -1,13 +1,92 @@
-﻿using System.Web.Mvc;
+﻿using ImageServiceWebApplication.Models;
+using System.Web.Mvc;
 
 namespace ImageServiceWebApplication.Controllers
 {
     public class PhotosController : Controller
     {
-        // GET: Photos
-        public ActionResult Index()
+        public static Photos photos = new Photos();
+        private static Photo m_currentPhoto;
+
+        /// <summary>
+        /// constructor.
+        /// </summary>
+        public PhotosController()
         {
-            return View();
+            photos.NotifyEvent -= Notify;
+            photos.NotifyEvent += Notify;
+
+        }
+
+        /// <summary>
+        /// Notify function.
+        /// notify view about update.
+        /// </summary>
+        void Notify()
+        {
+            Photos();
+        }
+
+        // GET: Photos
+        public ActionResult Photos()
+        {
+            photos.PhotosList.Clear();
+            photos.GetPhotos();
+            return View(photos.PhotosList);
+        }
+
+        /// <summary>
+        /// PhotosViewer function.
+        /// </summary>
+        /// <param name="photoRelPath"> the pic to be presented</param>
+        /// <returns></returns>
+        public ActionResult PhotosViewer(string photoRelPath)
+        {
+            UpdateCurrentPhotoFromRelPath(photoRelPath);
+            return View(m_currentPhoto);
+        }
+
+        /// <summary>
+        /// DeletePhoto function.
+        /// </summary>
+        /// <param name="photoRelPath"></param>
+        /// <returns></returns>
+        public ActionResult DeletePhoto(string photoRelPath)
+        {
+            UpdateCurrentPhotoFromRelPath(photoRelPath);
+            return View(m_currentPhoto);
+        }
+
+        /// <summary>
+        /// DeleteYes function.
+        /// confirmation of the delete.
+        /// </summary>
+        /// <param name="photoRelPath"></param>
+        /// <returns></returns>
+        public ActionResult DeleteYes(string photoRelPath)
+        {
+
+            System.IO.File.Delete(m_currentPhoto.ImageUrl);
+            System.IO.File.Delete(m_currentPhoto.ImageFullUrl);
+            photos.PhotosList.Remove(m_currentPhoto);
+            return RedirectToAction("Photos");
+        }
+
+        /// <summary>
+        /// UpdateCurrentPhotoFromRelPath function.
+        /// updates the current photo.
+        /// </summary>
+        /// <param name="photoRelPath"></param>
+        private void UpdateCurrentPhotoFromRelPath(string photoRelPath)
+        {
+            foreach (Photo photo in photos.PhotosList)
+            {
+                if (photo.ImageRelativePath == photoRelPath)
+                {
+                    m_currentPhoto = photo;
+                    break;
+                }
+            }
         }
     }
 }
