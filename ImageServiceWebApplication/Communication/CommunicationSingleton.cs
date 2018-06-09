448541420dafb0947333640e4a352a463da2c6e1
ShlomiZ <shlomi.z1991@gmail.com>
2018-06-09 20:04:17 +0300

    Bug fixed

--- ImageServiceWebApplication/Communication/CommunicationSingleton.cs
+++ ImageServiceWebApplication/Communication/CommunicationSingleton.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ImageServiceWebApplication.Communication
@@ -13,11 +14,12 @@ namespace ImageServiceWebApplication.Communication
     {
         const string IP = "127.0.0.1";
         const int PORT = 8000;
-
+        private static Mutex m_mutex = new Mutex();
+        private static Mutex m_readMutex = new Mutex();
         TcpClient client;
-        NetworkStream stream;
-        BinaryReader reader;
-        BinaryWriter writer;
+        //NetworkStream stream;
+        //BinaryReader reader;
+        //BinaryWriter writer;
         private static CommunicationSingleton instance;
         public event EventHandler<CommandRecievedEventArgs> InMessage;
         private bool isConnected;
@@ -63,9 +65,9 @@ namespace ImageServiceWebApplication.Communication
                 IPEndPoint ep = new IPEndPoint(IPAddress.Parse(IP), PORT);
                 client = new TcpClient();
                 client.Connect(ep);
-                stream = client.GetStream();
-                reader = new BinaryReader(stream);
-                writer = new BinaryWriter(stream);
+                //stream = client.GetStream();
+                //reader = new BinaryReader(stream);
+                //writer = new BinaryWriter(stream);
                 IsConnected = true;
             }
             // if error occurred
@@ -89,7 +91,9 @@ namespace ImageServiceWebApplication.Communication
                 NetworkStream stream = client.GetStream();
                 BinaryWriter writer = new BinaryWriter(stream);
                 //send json
+                m_mutex.WaitOne();
                 writer.Write(json);
+                m_mutex.ReleaseMutex();
             }
             catch (Exception e)
             {
@@ -109,8 +113,12 @@ namespace ImageServiceWebApplication.Communication
                 {
                     try
                     {
+                        NetworkStream stream = client.GetStream();
+                        BinaryReader reader = new BinaryReader(stream);
                         // read from server
+                        m_readMutex.WaitOne();
                         string temp = reader.ReadString();
+                        m_readMutex.ReleaseMutex();
                         if (temp != "")
                         {
                             // create command event args
@@ -142,6 +150,8 @@ namespace ImageServiceWebApplication.Communication
         /// </summary>
         public void Disconnect()
         {
+            CommandRecievedEventArgs commandRecievedEventArgs = new CommandRecievedEventArgs((int)CommandEnum.DisconnectClient, null, "");
+            this.Write(commandRecievedEventArgs);
             // close the connection
             client.Close();
             // change IsConnected to false
